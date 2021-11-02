cce10cab3ba625bc348188bd509c282dee8e068f
Muhammad Ridhwan Ginanjar Sutresna <ridhwangs@gmail.com>
2021-11-03 00:11:35 +0700

    merge

--- resources/views/antrian/antrian_show.blade.php
+++ resources/views/antrian/antrian_show.blade.php
@@ -11,7 +11,7 @@
 </style>
 @stop
 @section('content')
-<button class="btn btn-sm mb-3 btn-secondary btn-block rounded-0" onclick="windows.print();">Print</button>
+<button class="btn btn-sm mb-3 btn-secondary btn-block rounded-0" onclick="window.print();">Print</button>
     <section class="print-section text-center">
         <p>Nomor Antrian</p>
         <h1>{{ $data->nomor }}</h1>
