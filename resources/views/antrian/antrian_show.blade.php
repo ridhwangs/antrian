2bdfb6592788293975c5bba767ea1d19d698349f
Muhammad Ridhwan Ginanjar Sutresna <ridhwangs@gmail.com>
2021-11-02 22:28:31 +0700

    merge

--- resources/views/antrian/antrian_show.blade.php
+++ resources/views/antrian/antrian_show.blade.php
@@ -1,110 +1,23 @@
-@extends('layouts.app')
-@section('title', $data->antrian_id)
-@section('custom_style')
-<style>
-    body {
-        margin: 0;
-        font-family: "Nunito", sans-serif;
-        font-size: 0.9rem;
-        font-weight: 400;
-        line-height: 1.6;
-        color: #212529;
-        text-align: left;
-        background-color: #f8fafc !important;
-    }
-    @page { size: 58mm 100mm } /* output size */
-    body.receipt .sheet { width: 58mm; height: 100mm } /* sheet size */
-    @media print { body.receipt { width: 58mm } } /* fix for Chrome */
-</style>
-@stop
-@section('content')
-    <div class="row p-4 d-flex flex-column min-vh-100 justify-content-center align-items-center">
-        <div class="col-sm-4 mb-3">
-            <div class="card rounded-0">
-                <div class="card-header"><a class="btn btn-danger rounded-0 float-right" href="{{ route('antrian.create') }}?dealerID={{ $_GET['dealerID'] }}">Close</a></div>
-                <div class="card-body">
-                    <section class="print-section text-center">
-                        <p class="mb-0 text-center">PT. SURYAPUTRA SARANA</p>
-                        <p class="mb-0 text-center" style="font-size:8pt;">{{ $data->dealerID }}</p>
-                        <h1 class="mb-0 text-center" style="font-size:64pt;">{{ str_pad($data->nomor, 3, '0', STR_PAD_LEFT) }}</h1>
-                        <p class="mb-0 text-center">{{ $data->keterangan }}</p>
-                        <br><br><br><br>
-                        <p><sup>**terima kasih atas kunjungan anda**</sup></p>
-                        <br>
-                    </section>
-                </div>
-                <div class="card-footer">
-                    <button id="btn-print" onclick="printDiv('.print-section');" class="btn btn-primary btn-block rounded-0">Print</button>
-                </div>
-            </div>
-        </div>  
-    </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<button class="print">Print this page</button>
-@stop
-@section('custom_script')
-<script type="text/javascript">
-$(document).ready(function($) {
-  var ua = navigator.userAgent.toLowerCase();
-  var isAndroid = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
+<html xmlns="//www.w3.org/1999/xhtml">
+    <head>
+        <title>Print</title>
+    </head>
+    <body>
+        <div>
+            <p>On android devices (I have tested Nexus 5, Nexus 10, Galaxy S4 and Galaxy Tab 3) the window.print() command in javascript doesn't do anything, as far as I can tell it doesn't even register an error.</p>
+            <p>I know for a fact that most if not all of these browsers can print because you can use mobile chromes menu to choose "print".  My questions is, why doesn't window.print() trigger the behavior you would expect (opening the clients print menu).
+            And is there an android alternative to window.print()?</p>
+        </div>
 
-  $('button.print').click(function(e) {
-    e.preventDefault();
-    if (isAndroid) {
-      // https://developers.google.com/cloud-print/docs/gadget
-      var gadget = new cloudprint.Gadget();
-      gadget.setPrintDocument("url", $('title').html(), window.location.href, "utf-8");
-      gadget.openPrintDialog();
-    } else {
-      window.print();
-    }
-    return false;
-  });
-});
-$( document ).ready(function() {
-    $("#btn-print").click();
-});
-    function printDiv(elem) {
-        renderMe($('<div/>').append($(elem).clone()).html());
-    }
-    var popupBlockerChecker = {
-        check: function(popup_window){
-            var scope = this;
-            if (popup_window) {
-                if(/chrome/.test(navigator.userAgent.toLowerCase())){
-                    setTimeout(function () {
-                        scope.is_popup_blocked(scope, popup_window);
-                    },200);
-                }else{
-                    popup_window.onload = function () {
-                        scope.is_popup_blocked(scope, popup_window);
-                    };
-                }
-            } else {
-                scope.displayError();
-            }
-        },
-        is_popup_blocked: function(scope, popup_window){
-            if ((popup_window.innerHeight > 0)==false){ 
-                scope.displayError();
-            }
-        },
-        displayError: function(){
-            alert("Popup Blocker is enabled! Please add this site to your exception list.");
-        }
-    }
-    function renderMe(data) {
-        var mywindow = window.open('', 'print-section', 'height=' + screen.height + ',width=' + screen.width);
-        popupBlockerChecker.check(mywindow);
-        mywindow.document.write('<html><head><title></title>');
-        mywindow.document.write('</head><body >');
-        mywindow.document.write(data);
-        mywindow.document.write('</body></html>');
-        setTimeout(function () {
-            mywindow.print();
-            mywindow.close();
-        }, 1000)
-        return true;
-    }
-</script>
-@stop
+        <div id="gcpPrint"></div>
+
+        <script src="//www.google.com/cloudprint/client/cpgadget.js">
+        </script>
+
+        <script>
+            var gadget = new cloudprint.Gadget();
+            gadget.setPrintButton(cloudprint.Gadget.createDefaultPrintButton("gcpPrint"));
+            gadget.setPrintDocument("text/html", "Print", document.documentElement.innerHTML);
+        </script>
+    </body>
+</html>
