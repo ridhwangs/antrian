1c933af4ddc098daeaff631bee648a4258ebbd02
Muhammad Ridhwan Ginanjar Sutresna <ridhwangs@gmail.com>
2021-11-03 00:35:12 +0700

    merge

--- resources/views/antrian/antrian_show.blade.php
+++ resources/views/antrian/antrian_show.blade.php
@@ -14,9 +14,13 @@
         <p style="font-size:100pt;">Nomor Antrian</p>
         <h1 style="font-size:200pt;">{{ str_pad($data->nomor, 3, '0', STR_PAD_LEFT) }}</h1>
         <span style="font-size:70pt;">{{ $data->keterangan }}</span>
-        @for($i = 0; $i < 20; $i++)
+        @for($i = 0; $i < 30; $i++)
         <br>
         @endfor
         *****
 </section>
+<script type="text/javascript">
+setTimeout(function () { window.print(); }, 500);
+        window.onfocus = function () { setTimeout(function () { window.close(); }, 500); }
+</script>
 @stop
