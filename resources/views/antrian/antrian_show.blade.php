1d65aa0766363f21bf1939ee3518bfa2b1052d1f
Muhammad Ridhwan Ginanjar Sutresna <ridhwangs@gmail.com>
2021-11-03 00:22:55 +0700

    merge

--- resources/views/antrian/antrian_show.blade.php
+++ resources/views/antrian/antrian_show.blade.php
@@ -3,14 +3,15 @@
 @section('custom_style')
 <style>
     @media print { body { 
+        font-family: Arial, Helvetica, sans-serif;
         width: 58mm;
         } } /* fix for Chrome */
 </style>
 @stop
 @section('content')
-<button onclick="window.print();">Print</button>
+<section onclick="window.print();">
         <p style="font-size:100pt;">Nomor Antrian</p>
-        <h1 style="font-size:150pt;">{{ $data->nomor }}</h1>
+        <h1 style="font-size:200pt;">{{ $data->nomor }}</h1>
         <span style="font-size:100pt;">{{ $data->jenis_id }}</span>
- 
+</section>
 @stop
