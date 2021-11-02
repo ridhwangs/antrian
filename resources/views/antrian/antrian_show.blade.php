a1877cd1932edd76f2f8d048b2e044a9a30fe6c8
Muhammad Ridhwan Ginanjar Sutresna <ridhwangs@gmail.com>
2021-11-03 00:01:57 +0700

    merge

--- resources/views/antrian/antrian_show.blade.php
+++ resources/views/antrian/antrian_show.blade.php
@@ -2,19 +2,21 @@
 @section('title', $data->antrian_id)
 @section('custom_style')
 <style>
-    @media print (max-width: 767px) {
-   /* write css here for mobile responsive for print */
-}
+    body{
+        background:#FFFFFF;
+    }
+    @page { size: 58mm 100mm } /* output size */
+    body.receipt .sheet { width: 58mm; height: 100mm } /* sheet size */
+    @media print { body.receipt { width: 58mm } } /* fix for Chrome */
 </style>
 @stop
 @section('content')
-<button class="btn btn-sm mb-3 btn-secondary btn-block rounded-0" onclick="window.print()">Print</button>
+<button class="btn btn-sm mb-3 btn-secondary btn-block rounded-0" onclick="windows.print();">Print</button>
     <section class="print-section text-center">
         <p>Nomor Antrian</p>
         <h1>{{ $data->nomor }}</h1>
         <span>{{ $data->jenis_id }}</span>
     </section>
-    
 @stop
 @section('custom_script')
 
