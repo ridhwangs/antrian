a03da76ab528050791a16846a5501575932f5b6f
Muhammad Ridhwan Ginanjar Sutresna <ridhwangs@gmail.com>
2021-11-03 00:26:54 +0700

    merge

--- resources/views/antrian/antrian_show.blade.php
+++ resources/views/antrian/antrian_show.blade.php
@@ -13,6 +13,7 @@
 <section onclick="window.print();">
         <p style="font-size:100pt;">Nomor Antrian</p>
         <h1 style="font-size:200pt;">{{ str_pad($data->nomor, 3, '0', STR_PAD_LEFT) }}</h1>
-        <span style="font-size:100pt;">{{ $data->keterangan }}</span>
+        <span style="font-size:70pt;">{{ $data->keterangan }}</span>
+        <br><br><br><br><br>
 </section>
 @stop
