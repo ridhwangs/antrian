a16feb909ed2dca1c627314618c538a5f2b764a0
Muhammad Ridhwan Ginanjar Sutresna <ridhwangs@gmail.com>
2021-11-20 09:53:11 +0700

    merge

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,6 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" contnt="width=device-width, initial-scale=1.0">
     
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title') - PT. Suryaputra Sarana</title>
