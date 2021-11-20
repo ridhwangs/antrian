fdbc9d7567239fbb68da45b327c1ba465ecdf2c1
Muhammad Ridhwan Ginanjar Sutresna <ridhwangs@gmail.com>
2021-11-20 09:00:03 +0700

    merge

--- resources/views/antrian/antrian_create.blade.php
+++ resources/views/antrian/antrian_create.blade.php
@@ -19,51 +19,119 @@
             </div>
         </div>
 
-        <div class="col-sm-4">
+        <div class="col-sm-7">
             <div class="mb-3">
-                <input type="text" class="form-control input" onkeyup="getNoPol()" inputmode='none' id="tmp_no_pol" aria-describedby="tmp_no_pol" autofocus>
-                <div id="tmp_no_pol" class="form-text">Masukan No Polisi</div>
-                
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm p-0 pt-3">
+                            <input type="text" class="form-control input rounded-0" onkeyup="getNoPol()" inputmode='none' placeholder="No Kendaraan / No Polisi" id="tmp_no_pol" aria-describedby="tmp_no_pol">
+                        </div>
+                    </div>
+                </div>
+                <div class="card rounded-0" id="keyboard-nopol">
+                    <div class="card-body">
+                        <div class="container my-3" id='first' data-toggle="keybrd" data-target-input="#tmp_no_pol">
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <div class="row btn-group-lg mb-3" role='group'>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">Q</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">W</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">E</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">R</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">T</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">Y</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">U</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">I</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">O</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">P</button>
+                                    </div>
+
+                                    <div class="row btn-group-lg mb-3" role='group'>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">A</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">S</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">D</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">F</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">G</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">H</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">J</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">K</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">L</button>
+                                    </div>
+
+                                    <div class="row btn-group-lg mb-3" role='group'>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">Z</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">X</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">C</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">V</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">B</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">N</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">J</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">M</button>
+                                    </div>
+                                </div>
+                                <div class="col-md">
+                                    <div class="row btn-group-lg mb-3" role='group'>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">1</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">2</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3">3</button>
+                                    </div>
+                                    <div class="row btn-group-lg mb-3">
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">4</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">5</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3">6</button>
+                                    </div>
+                                    <div class="row btn-group-lg mb-3">
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">7</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">8</button>
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3">9</button>
+                                    </div>
+                                    <div class="row btn-group-lg mb-3">
+                                        <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:180px !important;">0</button>
+                                    </div>
+                                </div>
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="mb-3">
                <div class="container">
                     <div class="row">
                         <div class="col-sm p-0 pt-3">
-                            <input class="form-control form-control-lg my-input rounded-0"  onkeyup="getKilometer()" inputmode='none' id="tmp_kilometer" type="text" readonly>
+                            <input class="form-control form-control-lg my-input rounded-0"  onkeyup="getKilometer()"  placeholder="Kilometer Kendaraan" inputmode='none' id="tmp_kilometer" type="text">
                         </div>
                     </div>
                 </div>
-                <div class="card">
+                <div class="card rounded-0" id="keyboard-kilometer">
                     <div class="card-body">
-                        <div class="container my-3" id='second' data-toggle="keybrd" data-target-input=".my-input">
+                        <div class="container my-3" id='second' data-toggle="keybrd" data-target-input="#tmp_kilometer">
                             <div class="row btn-group-lg mb-3" role='group'>
-                                <button type="button" class="col-sm btn btn-secondary rounded-0 py-3 mr-3">1</button>
-                                <button type="button" class="col-sm btn btn-secondary rounded-0 py-3 mr-3">2</button>
-                                <button type="button" class="col-sm btn btn-secondary rounded-0 py-3">3</button>
+                                <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">1</button>
+                                <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">2</button>
+                                <button type="button" class="btn btn-secondary rounded-0 py-3">3</button>
                             </div>
                             <div class="row btn-group-lg mb-3">
-                                <button type="button" class="col-sm btn btn-secondary rounded-0 py-3 mr-3">4</button>
-                                <button type="button" class="col-sm btn btn-secondary rounded-0 py-3 mr-3">5</button>
-                                <button type="button" class="col-sm btn btn-secondary rounded-0 py-3">6</button>
+                                <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">4</button>
+                                <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">5</button>
+                                <button type="button" class="btn btn-secondary rounded-0 py-3">6</button>
                             </div>
                             <div class="row btn-group-lg mb-3">
-                                <button type="button" class="col-sm btn btn-secondary rounded-0 py-3 mr-3">7</button>
-                                <button type="button" class="col-sm btn btn-secondary rounded-0 py-3 mr-3">8</button>
-                                <button type="button" class="col-sm btn btn-secondary rounded-0 py-3">9</button>
+                                <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">7</button>
+                                <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:50px !important;">8</button>
+                                <button type="button" class="btn btn-secondary rounded-0 py-3">9</button>
                             </div>
                             <div class="row btn-group-lg mb-3">
-                                <button type="button" class="col-sm btn btn-secondary rounded-0 py-3 mr-3">0</button>
-                                <button type="button" class="col-sm-4 btn btn-danger rounded-0 py-3">Delete</button>
+                                <button type="button" class="btn btn-secondary rounded-0 py-3 mr-3" style="width:180px !important;">0</button>
                             </div>
                         </div>
                     </div>
                 </div>
-              
 
             </div>
         </div>
 
-        <div class="col-sm-8">
+        <div class="col-sm">
             @foreach($jenis_antrian AS $key => $rows)     
                 <div class="mb-4" onclick="document.getElementById('form-{{ $rows->jenis_id }}').submit();" style="cursor:pointer;">
                     <div class="card rounded-0">
@@ -100,16 +168,18 @@
         $(".kilometer").val(kilometer);
     }
     $('[data-toggle="keybrd"]').each(function(){
-	var btn = $(this).find('.btn'), input = $($(this).data('target-input'));
-	btn.on('click',function(e){
-		e.preventDefault();
-		var val = $(this).text();
-		if(val=='Delete'){
-			input.val(input.val().slice(0,-1));
-		}else{
-			input.val(input.val()+val);
-		}
-	});
-});
+        var btn = $(this).find('.btn'), input = $($(this).data('target-input'));
+        btn.on('click',function(e){
+            e.preventDefault();
+            var val = $(this).text();
+            if(val=='<-'){
+                input.val(input.val().slice(0,-1));
+            }else{
+                input.val(input.val()+val);
+                getNoPol();
+                getKilometer();
+            }
+        });
+    });
 </script>
 @stop
