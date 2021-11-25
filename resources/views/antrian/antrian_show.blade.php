662c66835c13ea7093f53afd8634b16db7ce4f45
Muhammad Ridhwan Ginanjar Sutresna <ridhwangs@gmail.com>
2021-11-25 11:51:44 +0700

    merge

--- resources/views/antrian/antrian_show.blade.php
+++ resources/views/antrian/antrian_show.blade.php
@@ -12,14 +12,14 @@
 @section('content')
         <img src="//www.ptsps.co.id/wp-content/uploads/2019/11/logo-sps.png" width="250px">
         <p style="font-size:50pt;">PT. SURYAPUTRA SARANA</p>
-        <p style="font-size:30pt;">{{ $data->dealerID }}</p>
+        <p style="font-size:40pt;">{{ $data->dealerID }}</p>
         <p style="font-size:50pt;">Nomor Antrian</p>
         <h1 style="font-size:280pt;"><b>{{ str_pad($data->nomor, 3, '0', STR_PAD_LEFT) }}</b></h1>
         @if(!empty($data->no_pol))
             <p style="font-size:70pt;">{{ $data->no_pol }}</p >
         @endif
         @if(!empty($data->kilometer))
-            <p style="font-size:30pt;">Current Mileage: {{ $data->kilometer }}</p>
+            <p style="font-size:50pt;">Current Mileage: {{ $data->kilometer }}</p>
         @endif
         <span style="font-size:70pt;">{{ $data->keterangan }}</span>
         @for($i = 0; $i < 10; $i++)
